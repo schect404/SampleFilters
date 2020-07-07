81930d5fee661286fd458e69a892f10642775a85
Томилович Илья Тарасович <AB070400TIT@IT2F-WIN-004.ws.a-bank.com.ua>
2020-07-07 10:54:34 +0300

    removing redundant file

--- filterAndroid/app/src/main/java/com/example/samplearchitecture/stub/StubNavigator.kt
+++
@@ -1,5 +0,0 @@
-package com.example.samplearchitecture.stub
-
-import androidx.fragment.app.FragmentManager
-import com.example.samplearchitecture.base.BaseNavigator
-import java.lang.ref.WeakReference
